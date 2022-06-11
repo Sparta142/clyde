a5a3e883cfe25e9838a85959847dcc5dfb8fdc91
Sparta142 <Sparta142@users.noreply.github.com>
2022-06-11 15:12:27 -0400

    Add docstrings to ApplicationCommandType

--- clyde/models/interactions.py
+++ clyde/models/interactions.py
@@ -31,8 +31,22 @@ class InteractionType(IntEnum):
 # https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
 class ApplicationCommandType(IntEnum):
     CHAT_INPUT = 1
+    """
+    Slash commands; a text-based command that
+    shows up when a user types ``/``
+    """
+
     USER = 2
+    """
+    A UI-based command that shows up when you
+    right click or tap on a user
+    """
+
     MESSAGE = 3
+    """
+    A UI-based command that shows up when you
+    right click or tap on a message
+    """
 
 
 # https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-resolved-data-structure
